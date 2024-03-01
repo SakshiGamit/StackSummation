df32e5754e0e4647ec60685de1a80e3d178f90ea
Sakshi <sakshigamit.avm@gmail.com>
2024-03-01 16:09:58 +0530

    added

--- ReactJS/table/src/App.js
+++ ReactJS/table/src/App.js
@@ -1,7 +1,13 @@
+import React from "react";
 import JsonDataDisplay from "./table";
 import "./index.css";
 import Card from "./Card";
 import Netdata from "./Netdata";
+
+let count = 1;
+function IncNum() {
+  count++;
+}
 
 function App() {
   return (
@@ -22,8 +28,11 @@ function App() {
           );
         })}
       </div>
+        <div className="btnInc">
+        <h1 className="count">{count}</h1>
+        <button className="btn" onClick={IncNum}>Click Me</button>
+        </div>
     </div>
   );
 }
-
 export default App;
--- ReactJS/table/src/index.js
+++ ReactJS/table/src/index.js
@@ -7,10 +7,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
 );
-
-// ReactDOM.render(
-//   <>
-//     <App />
-//   </>,
-//   document.getElementById("root")
-// );
